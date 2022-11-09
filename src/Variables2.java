db1d2f75b8d06f66abd8d073942314a4aab6717e
IVIaike <trainer.ekb@gmail.com>
2022-11-09 21:55:56 +0500

    Задание 1 выполнено

---
+++ src/Variables2.java
@@ -0,0 +1,25 @@
+public class Variables2 {
+    public static void main(String[] args) {
+
+        // Задание 1
+
+        int block = 123547;
+        System.out.println("Количество блоков, доставленных на стройплощадку " + block + " шт.");
+
+        byte board = 120;
+        System.out.println("Количество досок, доставленных на стройплощадку " + board + " шт.");
+
+        short brick = 25347;
+        System.out.println("Количество кирпичей, доставленных на стройплощадку " + brick + " шт.");
+
+        long selfTappingScrew = 1100000;
+        System.out.println("Количество саморезов, доставленных на стройплощадку " + selfTappingScrew + " шт.");
+
+        float concrete = 4.2f;
+        System.out.println("Количество бетона, доставленного на стройплощадку " + concrete + " куб.м.");
+
+        double water = 423.7;
+        System.out.println("Количество воды, доставленной на стройплощадку " + water + " куб.м.");
+
+    }
+}
